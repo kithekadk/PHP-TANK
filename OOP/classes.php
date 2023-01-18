fb322fc028e6f5c388f959004d2490725e5750d1
unknown <dankinyi99@gmail.com>
2023-01-18 11:22:43 +0300

    added constructor, getters and setters and inheritance

--- OOP/classes.php
+++ OOP/classes.php
@@ -1,10 +1,35 @@
 <?php
     class Person{
-        private $firstname = "Daniel";
-        private $lastname = "Kitheka";
+        //properties
+        protected $firstname = "Daniel";
+        protected $lastname = "Kitheka";
         private $age = 55;
     }
 
+    //inheritance
+    class Man extends Person{
+        private $hasBeards;
+
+        public function getManDetails(){
+            return $this->firstname;
+        }
+    }
+
+    // class without constructor
+    class Book{
+        var $title;
+        var $author;
+        var $pages;
+
+        function __construct($bTitle, $bAuthor, $bPages){
+            $this->title=$bTitle;
+            $this->author=$bAuthor;
+            $this->pages=$bPages;
+        }
+        
+    }
+
+    //Getters and setters
     class Pet{
         private function owner(){
             $sound = "Hi Boss";
@@ -14,4 +39,5 @@
             return $this->owner();
         }
     }
+
 ?>
--- OOP/index.php
+++ OOP/index.php
@@ -12,6 +12,13 @@
         $pet1 = new Pet();
 
         echo $pet1->getOwner();
+
+        $book1 = new Book("Maths Mastermind", "C Muturi", 300);
+
+        echo "$book1->title <br>";
+
+        $man1 = new Man();
+        echo $man1->getManDetails();
     ?>
 </body>
 </html>
